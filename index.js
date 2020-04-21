b08ddee75c2d4d8d4fe4d4bddd0744c5291de5c6
jackmorrison <jsm1317@ic.ac.uk>
2020-04-21 17:37:53 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+var cors = require('cors');
+const axios = require('axios');
+
+app.use(
+    cors({
+        credentials: true,
+        origin: true
+    })
+);
+app.options('*', cors());
+
+app.get('/', (req, res) => res.send('Working!!!'));
+
+app.get('/last_song', (req,res) => {
+
+
+  axios.get('https://api.github.com/repos/jackmorrison12/personal-website').then(resp => {
+    res.send(resp.data.stargazers_count.toString());
+  });
+
+});
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log('server running on port 3000', '');
+});
